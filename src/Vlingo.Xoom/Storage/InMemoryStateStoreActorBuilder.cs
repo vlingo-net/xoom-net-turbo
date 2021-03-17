65c3c3a8e485191574fa73df9d121f69dc3e37dc
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-03-18 00:44:36 +0100

    Remove generic type parameter from IStateStore

--- src/Vlingo.Xoom/Storage/InMemoryStateStoreActorBuilder.cs
+++ src/Vlingo.Xoom/Storage/InMemoryStateStoreActorBuilder.cs
@@ -17,7 +17,7 @@ namespace Vlingo.Xoom.Storage
     public class InMemoryStateStoreActorBuilder<T> : IStoreActorBuilder<T>
     {
         public T Build(Stage stage, IEnumerable<IDispatcher<Dispatchable<IEntry<T>, IState>>> dispatchers) => 
-            stage.ActorFor<T>(typeof(IStateStore<IEntry>), typeof(InMemoryStateStoreActor<IState, IEntry>), dispatchers);
+            stage.ActorFor<T>(typeof(IStateStore), typeof(InMemoryStateStoreActor<IState, IEntry>), dispatchers);
 
         public bool Support(DatabaseType databaseType) => databaseType.IsInMemory;
     }
