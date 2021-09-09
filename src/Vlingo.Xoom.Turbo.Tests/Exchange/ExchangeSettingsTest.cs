1db6661b49979836ae78240d67fa4a2ff22209a8
Hamza JGUERIM <hamzajg@gmail.com>
2021-09-09 18:09:34 +0100

    Add ExchangeSettings Test..
    Signed-off-by: Hamza JGUERIM <hamzajg@gmail.com>

---
+++ src/Vlingo.Xoom.Turbo.Tests/Exchange/ExchangeSettingsTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+using NuGet.Frameworks;
+using Vlingo.Xoom.Turbo.actors;
+using Vlingo.Xoom.Turbo.Exchange;
+using Xunit;
+
+namespace Vlingo.Xoom.Turbo.Tests.Exchange
+{
+  public class ExchangeSettingsTest
+  {
+    public ExchangeSettingsTest()
+    {
+      ExchangeSettings.Load(new ReadOnlyDictionary<string, string>(Settings.Properties()));
+    }
+
+    [Fact]
+    public void TestThatConnectionSettingsAreMapped()
+    {
+      var firstSettings = ExchangeSettings.Of("first").MapToConnection();
+
+      Assert.Equal("first-exchange", firstSettings.HostName);
+      Assert.Equal("vlingo01", firstSettings.Username);
+      Assert.Equal("vlingo-pass01", firstSettings.Password);
+      Assert.Equal("virtual-first-exchange", firstSettings.VirtualHost);
+      Assert.Equal(1000, firstSettings.Port);
+
+      var secondSettings = ExchangeSettings.Of("second").MapToConnection();
+
+      Assert.Equal("second-exchange", secondSettings.HostName);
+      Assert.Equal("vlingo02", secondSettings.Username);
+      Assert.Equal("vlingo-pass02", secondSettings.Password);
+      Assert.Equal("virtual-second-exchange", secondSettings.VirtualHost);
+      Assert.Equal(1001, secondSettings.Port);
+
+      Assert.Equal(2, ExchangeSettings.All().Count);
+    }
+  }
+}
--- src/Vlingo.Xoom.Turbo/Exchange/ExchangeSettings.cs
+++ src/Vlingo.Xoom.Turbo/Exchange/ExchangeSettings.cs
@@ -8,84 +8,93 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Vlingo.Xoom.Lattice.Exchange;
 
 namespace Vlingo.Xoom.Turbo.Exchange
 {
-    public class ExchangeSettings
+  public class ExchangeSettings
+  {
+    private static readonly string _exchangeNames = "exchange.names";
+    private static readonly List<ExchangeSettings> _allExchangeParameters = new List<ExchangeSettings>();
+    private static readonly List<string> _propertiesKeys = new List<string>()
     {
-        private static readonly string _exchangeNames = "exchange.names";
-        private static readonly List<ExchangeSettings> _allExchangeParameters = new List<ExchangeSettings>();
-        private static readonly List<string> _propertiesKeys = new List<string>()
-        {
-            "exchange.%s.hostname",
-            "exchange.%s.username",
-            "exchange.%s.password",
-            "exchange.%s.port",
-            "exchange.%s.virtual.host"
-        };
-        private readonly string _exchangeName;
-        private readonly List<string> _keys;
-        private readonly List<ExchangeSettingsItem> _parameters;
+      "exchange.{0}.hostname",
+      "exchange.{0}.username",
+      "exchange.{0}.password",
+      "exchange.{0}.port",
+      "exchange.{0}.virtual.host"
+    };
 
-        public static List<ExchangeSettings> All() => _allExchangeParameters;
+    private readonly string _exchangeName;
+    private readonly List<string> _keys;
+    private readonly List<ExchangeSettingsItem> _parameters;
 
-        public static ExchangeSettings Of(string exchangeName) => _allExchangeParameters.FirstOrDefault(param => param.HasName(exchangeName)) ?? throw new ArgumentException(string.Concat("Exchange with name ", exchangeName, " not found"));
+    public static List<ExchangeSettings> All() => _allExchangeParameters;
 
-        public List<ExchangeSettings> Load(IReadOnlyDictionary<string,string> properties)
-        {
-            if (_allExchangeParameters != null && _allExchangeParameters.Count == 0)
-            {
-                var exchangeParameters = ApplicationProperty.ReadMultipleValues(_exchangeNames, ";", properties).Select(x => new ExchangeSettings(_exchangeName, properties));
-                _allExchangeParameters.AddRange(exchangeParameters);
-            }
-            return _allExchangeParameters ?? new List<ExchangeSettings>();
-        }
+    public static ExchangeSettings Of(string exchangeName) =>
+      _allExchangeParameters.FirstOrDefault(param => param.HasName(exchangeName)) ??
+      throw new ArgumentException(string.Concat("Exchange with name ", exchangeName, " not found"));
 
-        private ExchangeSettings(string exchangeName, IReadOnlyDictionary<string, string> properties)
-        {
-            _exchangeName = exchangeName;
-            _keys = PrepareKeys(exchangeName);
-            _parameters = RetrieveParameters(properties);
-            Validate();
-        }
+    public static List<ExchangeSettings> Load(IReadOnlyDictionary<string, string> properties)
+    {
+      if (!_allExchangeParameters.Any())
+      {
+        Func<string, ExchangeSettings> mapper = exchangeNames => new ExchangeSettings(exchangeNames, properties);
 
-        private List<string> PrepareKeys(string exchangeName) => _propertiesKeys.Select(key => string.Format(key, exchangeName)).ToList();
+        var exchangeParameters = ApplicationProperty
+          .ReadMultipleValues(_exchangeNames, ";", properties).Select(mapper);
+        _allExchangeParameters.AddRange(exchangeParameters);
+      }
 
-        private void Validate()
-        {
-            var parametersNotFound = _parameters.Where(param => param.value == null).Select(param => param.key).ToList();
-            if (parametersNotFound != null && parametersNotFound.Count != 0)
-            {
-                throw new ExchangeSettingsNotFoundException(parametersNotFound);
-            }
-        }
-
-        private List<ExchangeSettingsItem> RetrieveParameters(IReadOnlyDictionary<string, string> properties) => _keys.Select(key => new ExchangeSettingsItem(key, ApplicationProperty.ReadValue(key, properties) ?? string.Empty)).ToList();
-
-        private bool HasName(string exchangeName) => _exchangeName == exchangeName;
-
-        public List<string> MapToConnection()
-        {
-            return new List<string>()
-            {
-                RetrieveParameterValue("hostname"),
-                RetrieveParameterValue("port"),
-                RetrieveParameterValue("virtual.host"),
-                RetrieveParameterValue("username"),
-                RetrieveParameterValue("password"),
-
-            };
-        }
-
-        private string RetrieveParameterValue(string key) => RetrieveParameterValue(key, value => value);
-
-        private T RetrieveParameterValue<T>(string keySuffix, Func<string, T> converter)
-        {
-            var key = ResolveKey(keySuffix);
-            var value = _parameters.Where(param => param.HasKey(key)).Select(param => param.value).First();
-            return converter(value);
-        }
-
-        private string ResolveKey(string keySuffix) => string.Format("exchange.%s.%s", _exchangeName, keySuffix);
+      return _allExchangeParameters ?? new List<ExchangeSettings>();
     }
+
+    private ExchangeSettings(string exchangeName, IReadOnlyDictionary<string, string> properties)
+    {
+      _exchangeName = exchangeName;
+      _keys = PrepareKeys(exchangeName);
+      _parameters = RetrieveParameters(properties);
+      
+      Validate();
+    }
+
+    private List<string> PrepareKeys(string exchangeName) => _propertiesKeys.Select(key => string.Format(key, exchangeName)).ToList();
+
+    private void Validate()
+    {
+      var parametersNotFound = _parameters
+        .Where(param => param.value == null)
+        .Select(param => param.key)
+        .ToList();
+      if (parametersNotFound != null && parametersNotFound.Any())
+      {
+        throw new ExchangeSettingsNotFoundException(parametersNotFound);
+      }
+    }
+
+    private List<ExchangeSettingsItem> RetrieveParameters(IReadOnlyDictionary<string, string> properties) 
+      => _keys
+      .Select(key => new ExchangeSettingsItem(key, ApplicationProperty.ReadValue(key, properties) ?? string.Empty))
+      .ToList();
+
+    private bool HasName(string exchangeName) => _exchangeName == exchangeName;
+
+    public ConnectionSettings MapToConnection() => ConnectionSettings.Instance(RetrieveParameterValue("hostname"),
+      RetrieveParameterValue("port", int.Parse), RetrieveParameterValue("virtual.host"),
+      RetrieveParameterValue("username"), RetrieveParameterValue("password"));
+
+    private string RetrieveParameterValue(string key) => RetrieveParameterValue(key, value => value);
+
+    private T RetrieveParameterValue<T>(string keySuffix, Func<string, T> converter)
+    {
+      var key = ResolveKey(keySuffix);
+      var value = _parameters
+        .Where(param => param.HasKey(key))
+        .Select(param => param.value)
+        .First();
+      return converter(value);
+    }
+
+    private string ResolveKey(string keySuffix) => $"exchange.{_exchangeName}.{keySuffix}";
+  }
 }
