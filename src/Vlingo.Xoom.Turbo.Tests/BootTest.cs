fd84140f5fa2ed62c606ca787b8d46ffb28d208d
Hamza JGUERIM <hamzajg@gmail.com>
2021-09-09 08:27:28 +0100

    Add BootTest..
    Signed-off-by: Hamza JGUERIM <hamzajg@gmail.com>

---
+++ src/Vlingo.Xoom.Turbo.Tests/BootTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace Vlingo.Xoom.Turbo.Tests
+{
+    public class BootTest
+    {
+
+        private const string BOOT_WORLD_NAME = "test-boot";
+
+        [Fact]
+        public void TestThatWorldStarts()
+        {
+            var world = Boot.Start(BOOT_WORLD_NAME);
+
+            Assert.Equal(BOOT_WORLD_NAME, world.Name);
+        }
+    }
+}
