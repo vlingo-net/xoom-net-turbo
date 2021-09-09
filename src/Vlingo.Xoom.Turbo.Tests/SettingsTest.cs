4947da8b1cb0bc9b3f734f111b0798e6189d00ac
Hamza JGUERIM <hamzajg@gmail.com>
2021-09-09 15:59:43 +0100

    Add SettingsTest..
    Signed-off-by: Hamza JGUERIM <hamzajg@gmail.com>

---
+++ src/Vlingo.Xoom.Turbo.Tests/SettingsTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Vlingo.Xoom.Turbo.actors;
+using Xunit;
+
+namespace Vlingo.Xoom.Turbo.Tests
+{
+    public class SettingsTest
+    {
+        [Fact]
+        public void TestThatSettingsAreLoadedForBlockingMailbox()
+        {
+            Assert.Equal(23, Settings.Properties().Count);
+        }
+    }
+}
