78984c00ec3dbe3bc41a1d456027598b0be12893
Hamza JGUERIM <hamzajg@gmail.com>
2021-09-09 09:04:49 +0100

    Add ComponentRegistry..
    Signed-off-by: Hamza JGUERIM <hamzajg@gmail.com>

---
+++ src/Vlingo.Xoom.Turbo.Tests/ComponentRegistryTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+
+namespace Vlingo.Xoom.Turbo.Tests
+{
+    public class ComponentRegistryTest : IDisposable
+    {
+        [Fact]
+        public void TestThatComponentsAreRegistered()
+        {
+            ComponentRegistry.Register("appName", "xoom-app");
+            ComponentRegistry.Register(typeof(bool), bool.TrueString);
+
+            Assert.True(ComponentRegistry.Has("appName"));
+            Assert.True(ComponentRegistry.Has(typeof(bool)));
+
+            Assert.False(ComponentRegistry.Has("component"));
+            Assert.False(ComponentRegistry.Has(typeof(int)));
+
+            Assert.Equal("xoom-app", ComponentRegistry.WithName("appName"));
+            Assert.Equal(bool.TrueString, ComponentRegistry.WithType(typeof(bool)));
+        }
+
+        public void Dispose()
+        {
+            ComponentRegistry.Clear();
+        }
+    }
+}
---
+++ src/Vlingo.Xoom.Turbo/ComponentRegistry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vlingo.Xoom.Turbo
+{
+    public class ComponentRegistry
+    {
+        private static readonly IDictionary<string, object> COMPONENTS = new Dictionary<string, object>();
+
+        public static void Register(string componentName, object componentInstance)
+        {
+            COMPONENTS.Add(componentName, componentInstance);
+        }
+
+        public static void Register(Type componentClass, object componentInstance)
+        {
+            COMPONENTS.Add(componentClass.FullName, componentInstance);
+        }
+
+        public static bool Has(Type componentClass) => COMPONENTS.ContainsKey(componentClass.FullName);
+
+        public static object WithName(string name) => COMPONENTS[name];
+
+        public static object WithType(Type componentClass) => WithName(componentClass.FullName);
+
+        public static bool Has(string componentName) => COMPONENTS.ContainsKey(componentName);
+
+        public static void Clear()
+        {
+            COMPONENTS.Clear();
+        }
+    }
+}
