912db05abe24524249997b7e232d4584f173658b
Hamza JGUERIM <hamzajg@gmail.com>
2021-09-10 19:32:15 +0100

    Add copyright notices
    Signed-off-by: Hamza JGUERIM <hamzajg@gmail.com>

--- src/Vlingo.Xoom.Turbo.Tests/BootTest.cs
+++ src/Vlingo.Xoom.Turbo.Tests/BootTest.cs
@@ -9,17 +9,16 @@ using Xunit;
 
 namespace Vlingo.Xoom.Turbo.Tests
 {
-    public class BootTest
+  public class BootTest
+  {
+    private const string BOOT_WORLD_NAME = "test-boot";
+
+    [Fact]
+    public void TestThatWorldStarts()
     {
+      var world = Boot.Start(BOOT_WORLD_NAME);
 
-        private const string BOOT_WORLD_NAME = "test-boot";
-
-        [Fact]
-        public void TestThatWorldStarts()
-        {
-            var world = Boot.Start(BOOT_WORLD_NAME);
-
-            Assert.Equal(BOOT_WORLD_NAME, world.Name);
-        }
+      Assert.Equal(BOOT_WORLD_NAME, world.Name);
     }
+  }
 }
