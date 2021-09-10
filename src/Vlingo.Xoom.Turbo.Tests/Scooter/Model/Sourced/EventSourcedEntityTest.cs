860900ba95f6498948dabcfc5c902b2ffc3d4f10
Hamza JGUERIM <hamzajg@gmail.com>
2021-09-10 19:24:00 +0100

    Add Event Sourced Entity Tests
    Signed-off-by: Hamza JGUERIM <hamzajg@gmail.com>

---
+++ src/Vlingo.Xoom.Turbo.Tests/Scooter/Model/Sourced/EventSourcedEntityTest.cs
@@ -0,0 +1,114 @@
+using System;
+using Vlingo.Xoom.Lattice.Model;
+using Vlingo.Xoom.Turbo.Scooter.Model.Sourced;
+using Xunit;
+
+namespace Vlingo.Xoom.Turbo.Tests.Scooter.Model.Sourced
+{
+  public class EventSourcedEntityTest
+  {
+    [Fact(Skip = "WIP")]
+    public void TestProductDefinedEventKept()
+    {
+      var product = new Product("dice", "fuz", "dice-fuz-1", "Fuzzy dice", 999);
+
+      Assert.Equal(3, product.Applied().Size());
+    }
+
+    public class Product : ProductParent
+    {
+      public string Name { get; set; }
+      public string Description { get; set; }
+      public long Price { get; set; }
+
+      public Product(string type, string category, string name, string description, long price) : base(type, category)
+      {
+        Name = name;
+        Description = description;
+        Price = price;
+
+        Apply(new ProductDefined(name, description, price));
+      }
+
+      public class ProductDefined : DomainEvent
+      {
+        public string Name { get; }
+        public string Description { get; }
+        public long Price { get; }
+        public DateTime OccurredOn { get; }
+        public int Version { get; }
+
+        public ProductDefined(string name, string description, long price)
+        {
+          Name = name;
+          Description = description;
+          Price = price;
+          OccurredOn = DateTime.Now;
+          Version = 1;
+        }
+      }
+
+      public override string Id()
+      {
+        throw new System.NotImplementedException();
+      }
+
+      protected override string StreamName()
+      {
+        throw new System.NotImplementedException();
+      }
+    }
+
+    public abstract class ProductParent : ProductGrandParent
+    {
+      public string Category { get; set; }
+
+      protected ProductParent(string type, string category) : base(type)
+      {
+        Category = category;
+
+        Apply(new ProductParentCategorized(category));
+      }
+
+      public class ProductParentCategorized : DomainEvent
+      {
+        public string Category { get; }
+        public DateTime OccurredOn { get; }
+        public int Version { get; }
+
+        public ProductParentCategorized(string category)
+        {
+          Category = category;
+          OccurredOn = DateTime.Now;
+          Version = 1;
+        }
+      }
+    }
+
+    public abstract class ProductGrandParent : EventSourcedEntity
+    {
+      public string Type { get; set; }
+
+      protected ProductGrandParent(string type)
+      {
+        Type = type;
+
+        Apply(new ProductGrandParentTyped(type));
+      }
+
+      public class ProductGrandParentTyped : DomainEvent
+      {
+        public string Type { get; }
+        public DateTime OccurredOn { get; }
+        public int Version { get; }
+
+        public ProductGrandParentTyped(string type)
+        {
+          Type = type;
+          OccurredOn = DateTime.Now;
+          Version = 1;
+        }
+      }
+    }
+  }
+}
