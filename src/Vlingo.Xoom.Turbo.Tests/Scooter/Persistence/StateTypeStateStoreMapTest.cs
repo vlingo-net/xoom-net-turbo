fee524440cfdde28772f1fc8894f20a6b98cd46c
Hamza JGUERIM <hamzajg@gmail.com>
2021-09-16 13:19:46 +0100

    Add StateType StateStoreMap Test
    Signed-off-by: Hamza JGUERIM <hamzajg@gmail.com>

---
+++ src/Vlingo.Xoom.Turbo.Tests/Scooter/Persistence/StateTypeStateStoreMapTest.cs
@@ -0,0 +1,25 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using Vlingo.Xoom.Symbio.Store.State;
+using Vlingo.Xoom.Turbo.Tests.Scooter.Model.Stateful;
+using Xunit;
+
+namespace Vlingo.Xoom.Turbo.Tests.Scooter.Persistence
+{
+	public class StateTypeStateStoreMapTest
+	{
+		[Fact]
+		public void TestExistingMappings()
+		{
+			StateTypeStateStoreMap.StateTypeToStoreName(typeof(IEntity1).FullName, typeof(IEntity1));
+
+			Assert.Equal(typeof(IEntity1).FullName, StateTypeStateStoreMap.StoreNameFrom(typeof(IEntity1)));
+			Assert.Equal(typeof(IEntity1).FullName, StateTypeStateStoreMap.StoreNameFrom(typeof(IEntity1).FullName));
+		}
+	}
+}
